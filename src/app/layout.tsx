27d89fab93e7a513956c16a6fd917cec7e30e9b5
zee0401 <41227625+zee0401@users.noreply.github.com>
2024-11-19 21:04:18 +0530

    search form

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
-import Navbar from "./components/Navbar";
+import Navbar from "../components/Navbar";
 
 const workSans = localFont({
   src: [
---
+++ src/components/SearchForm.tsx
@@ -0,0 +1,26 @@
+import Form from "next/form";
+import { Search } from "lucide-react";
+import SearchFormReset from "./SearchFormReset";
+
+const SearchForm = ({ query }: { query: string }) => {
+  console.log(query);
+  return (
+    <Form action="/" scroll={false} className="search-form">
+      <input
+        className="search-input"
+        name="query"
+        defaultValue={query}
+        placeholder="Search Startups"
+      />
+      <div className="flex gap-2">
+        {query && <SearchFormReset />}
+
+        <button type="submit" className="search-btn text-white">
+          <Search className="size-5" />
+        </button>
+      </div>
+    </Form>
+  );
+};
+
+export default SearchForm;
