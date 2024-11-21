70e1ddbf52e98b2a856c9ae6645788c0003e54e3
zee0401 <41227625+zee0401@users.noreply.github.com>
2024-11-21 14:29:04 +0530

    get by id query

--- src/sanity/lib/queries.ts
+++ src/sanity/lib/queries.ts
@@ -14,3 +14,19 @@ export const STARTUPS_QUERY =
   category,
   image,
 }`);
+
+export const STARTUP_BY_ID_QUERY =
+  defineQuery(`*[_type == "startup" && _id == $id][0]{
+  _id, 
+  title, 
+  slug,
+  _createdAt,
+  author -> {
+    _id, name, username, image, bio
+  }, 
+  views,
+  description,
+  category,
+  image,
+  pitch,
+}`);
