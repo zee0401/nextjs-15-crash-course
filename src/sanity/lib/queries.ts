555557a421ca64c9acd91af2e7708ae5525e8852
zee0401 <41227625+zee0401@users.noreply.github.com>
2024-11-22 18:23:43 +0530

    sanity

--- src/sanity/lib/queries.ts
+++ src/sanity/lib/queries.ts
@@ -1,18 +1,18 @@
 import { defineQuery } from "next-sanity";
 
 export const STARTUPS_QUERY =
-  defineQuery(`*[_type == "startup" && defined(slug.current) && !defined($search) || title match $search || title match "^{$search}*" || category match $search || category match "^{$search}*" || author->name match $search || author->name match "^{$search}*"] | order(_createdAt desc) {  
-  _id,  
-  title,  
-  slug,  
-  _createdAt,  
-  author -> {  
-   _id, name, image, bio  
-  },  
-  views,  
-  description,  
-  category,  
-  image,  
+  defineQuery(`*[_type == "startup" && defined(slug.current) && !defined($search) || title match $search || category match $search || author->name match $search] | order(_createdAt desc) {
+  _id, 
+  title, 
+  slug,
+  _createdAt,
+  author -> {
+    _id, name, image, bio
+  }, 
+  views,
+  description,
+  category,
+  image,
 }`);
 
 export const STARTUP_BY_ID_QUERY =
