07aa326a15f43e7fdf278afc42bff2112ac1f2e1
zee0401 <41227625+zee0401@users.noreply.github.com>
2024-11-26 12:32:23 +0530

    callbacks for auth

--- auth.ts
+++ auth.ts
@@ -1,6 +1,48 @@
+import { client } from "@/sanity/lib/client";
+import { STARTUP_BY_GITHUB_ID_QUERY } from "@/sanity/lib/queries";
+import { writeClient } from "@/sanity/lib/writeClient";
 import NextAuth from "next-auth";
 import GitHub from "next-auth/providers/github";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   providers: [GitHub],
+  callbacks: {
+    async signIn(user: { name; email; image }, profile: { id; bio; login }) {
+      const existingUser = await client
+        .withConfig({ useCdn: false })
+        .fetch(STARTUP_BY_GITHUB_ID_QUERY, {
+          id,
+        });
+
+      if (!existingUser) {
+        await writeClient.create({
+          _type: "author",
+          id,
+          name,
+          username: login,
+          email,
+          image,
+          bio: bio || "",
+        });
+      }
+      return true;
+    },
+    async jwt({ token, account, profile }) {
+      if (account && profile) {
+        const user = await client
+          .withConfig({ useCdn: false })
+          .fetch(STARTUP_BY_GITHUB_ID_QUERY, {
+            id: profile?.id,
+          });
+
+        token.id = user?._id;
+      }
+
+      return token;
+    },
+    async session({ session, token }) {
+      Object.assign(session, { id: token.id });
+      return session;
+    },
+  },
 });
