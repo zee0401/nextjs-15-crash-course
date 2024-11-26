c0bc4bf357e9604f773724fd3335d5f8f0fc403f
zee0401 <41227625+zee0401@users.noreply.github.com>
2024-11-26 12:11:08 +0530

    github auth query

--- src/components/Views.tsx
+++ src/components/Views.tsx
@@ -2,12 +2,21 @@ import React from "react";
 import Ping from "./Ping";
 import { client } from "@/sanity/lib/client";
 import { STARTUP_BY_VIEWS_QUERY } from "@/sanity/lib/queries";
+import { writeClient } from "@/sanity/lib/writeClient";
+import { unstable_after as after } from "next/server";
 
 const Views = async ({ id }: { id: string }) => {
   const { views: totalViews } = await client
     .withConfig({ useCdn: false })
     .fetch(STARTUP_BY_VIEWS_QUERY, { id });
 
+  after(
+    async () =>
+      await writeClient
+        .patch(id)
+        .set({ views: totalViews + 1 })
+        .commit()
+  );
   return (
     <div className="view-container">
       <div className="absolute -top-2 -right-2">
--- src/sanity/lib/queries.ts
+++ src/sanity/lib/queries.ts
@@ -34,3 +34,14 @@ export const STARTUP_BY_ID_QUERY =
 export const STARTUP_BY_VIEWS_QUERY =
   defineQuery(`*[_type== "startup" && _id == $id]
 [0]{_id,views}`);
+
+export const STARTUP_BY_GITHUB_ID_QUERY =
+  defineQuery(`*[_type == "author" && id == $id][0]{
+  _id,
+  id, 
+  name, 
+  username, 
+  image,
+  email, 
+  bio
+}`);
