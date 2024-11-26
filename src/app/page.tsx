8b25efaec2dc9d9a1ba2dbcb148b20d1284e64e5
zee0401 <41227625+zee0401@users.noreply.github.com>
2024-11-26 15:03:26 +0530

    AUTH

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import SearchForm from "@/components/SearchForm";
 import StartupCard, { StartupTypeCard } from "@/components/StartupCard";
 import { sanityFetch, SanityLive } from "@/sanity/lib/live";
 import { STARTUPS_QUERY } from "@/sanity/lib/queries";
+import { auth } from "../../auth";
 
 export default async function Home({
   searchParams,
@@ -11,6 +12,8 @@ export default async function Home({
   const query = (await searchParams).query;
 
   const params = { search: query || null };
+
+  const session = await auth();
 
   const { data: posts } = await sanityFetch({ query: STARTUPS_QUERY, params });
 
