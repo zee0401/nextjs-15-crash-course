bf1972d1e53beb8cff0a7ed4fb6af585587e468f
zee0401 <41227625+zee0401@users.noreply.github.com>
2024-11-28 16:04:42 +0530

    skeleton added

---
+++ src/components/StartupCardSkeleton.tsx
@@ -0,0 +1,12 @@
+import { cn } from "@/lib/utils";
+import { Skeleton } from "./ui/skeleton";
+
+export const StartupCardSkeleton = () => (
+  <>
+    {[0, 1, 2, 3, 4].map((index: number) => (
+      <li key={cn("skeleton", index)}>
+        <Skeleton className="startup-card_skeleton" />
+      </li>
+    ))}
+  </>
+);
