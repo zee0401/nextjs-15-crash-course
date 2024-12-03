8c68fe9d4f75a2a3b98a72839667aa8c32ee6367
zee0401 <41227625+zee0401@users.noreply.github.com>
2024-12-03 12:28:03 +0530

    playlist by slug added

--- src/sanity/lib/queries.ts
+++ src/sanity/lib/queries.ts
@@ -71,3 +71,28 @@ export const AUTHOR_STARTUP_QUERY =
   category,
   image,
   }`);
+
+export const PLAYLIST_BY_SLUG_QUERY =
+  defineQuery(`*[_type == "playlist" && slug.current == $slug][0]{
+  _id,
+  title,
+  slug,
+  select[]->{
+    _id,
+    _createdAt,
+    title,
+    slug,
+    author->{
+      _id,
+      name,
+      slug,
+      image,
+      bio
+    },
+    views,
+    description,
+    category,
+    image,
+    pitch
+  }
+}`);
